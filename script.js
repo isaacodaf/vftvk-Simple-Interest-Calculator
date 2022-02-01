293c323d88b3a12f32ebea6d4d8b019d4edd55f7
Isaac Ogunrinde <isaacodaf@gmail.com>
2022-02-01 13:50:07 -0500

    changes made from my desktop

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
+// returns the range value whe the range slider is moved
+function sliderInput(rangeVal) {
+    document.querySelector('#rangeDisplay').value=rangeVal + '%'; 
+}
+
+ // Restart or Reset Calculator.
+ function resetCalculator() {
+  window.location.reload(true);
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+  let pAmount = document.querySelector('#principal').value;
+  pAmount = parseInt(pAmount); /*converts user input to a string, parses that string, then returns an integer or NaN */
+
+  console.log(pAmount)
+
+  if (pAmount <= 0) {         /*check if user entered a number greater than zero */
+      alert('You have made an invalid entry.\n\nPlease enter a number greater than zero.');
+      document.querySelector('#principal').focus();
+      
+    } else if (isNaN(pAmount) ) {     /*check if user entered is not a Number */
+      alert('The amount you entered is not a Number!\n\nPlease enter a number greater than zero.');
+      document.querySelector('#principal').focus();
+
+    } else {     /*check if this the correct number you want to enter*/
+      let userAnswer = prompt('The amount you entered is: '  + pAmount + '. \n\nIs that the amount you wanted to enter? If yes enter yes or y then click ok, and if not just click ok.\n\n');
+
+      // convert all user answers to uppercase input to lower case
+      let answer = userAnswer.toLowerCase();
+
+      if(answer !== 'yes' && answer !== 'y' ) {
+        document.querySelector('#principal').focus();
+
+      } else {
+        let interestRate = document.querySelector('#rate').value;
     
+  
+        let numYears = document.querySelector('#years').value;
+        console.log(numYears)
+      
+        let interestYielded = (pAmount)*parseInt(interestRate)*parseInt(numYears)/100;
+      
+        let receivedAmount = pAmount + interestYielded;
+      
+        console.log(receivedAmount)
+      
+        currentYear = new Date().getFullYear();
+        let year = parseInt(currentYear) + parseInt(numYears);
+      
+        console.log(year)
+      
+        // if user input is greater than zero then print message
+        if (pAmount > 0) {
+          // create output message
+          let response = "If you deposite <span>" + pAmount +
+          "</span>,<br> at an interest rate of <span>" + interestRate + '%' +
+          "</span>.<br><br> You will recieve an amount of <span>" + receivedAmount +
+          "</span>,<br>in the year <span>" + numYears + "<span>.";
+      
+          console.log(response)
+      
+          // returns the variable response in the form of HTML content to element span with id "displayResult"
+          document.querySelector('#displayResult').innerHTML = response;
+        }
+      }
+  }
+
+  
 }
         
